222268760dff142351c360b0c435753c4a6ad818
meri <mdiaz>
2020-03-09 21:38:26 +0100

    arreglando cosas con pep8

--- practica1.py
+++ practica1.py
@@ -28,11 +28,13 @@ url_pinchable = """
 
 """
 
+
 def create_entradas(content):
     entradas_html = ""
     for k, v in content.items():
         entradas_html += '<p>' + k + ': ' + v + '</p>\n'
     return entradas_html
+
 
 class contentApp (webapp.webApp):
     """Simple web application for managing content.
@@ -42,11 +44,11 @@ class contentApp (webapp.webApp):
 
     # Declare and initialize content
     short_url = {}
-    long_url= {}
+    long_url = {}
 
     def parse(self, request):
         """Return the resource name (including /)"""
-        request = request.replace("%2F",'/').replace("%3A", ':')
+        request = request.replace("%2F", '/').replace("%3A", ':')
         method = request.split(' ', 1)[0]
         resource = request.split(' ', 2)[1]
         if method == "POST":
@@ -58,6 +60,7 @@ class contentApp (webapp.webApp):
 
     def process(self, parsedRequest):
         """Process the relevant elements of the request.
+sudo apt install python-pip
 
         Finds the HTML text corresponding to the resource name,
         ignoring requests for resources not in the dictionary.
@@ -68,10 +71,12 @@ class contentApp (webapp.webApp):
             if resource == '/':
                 httpCode = "200 OK"
                 entradas_html = create_entradas(self.short_url)
-                htmlBody = "<html><body>" + formulario+ entradas_html +"</body></html>"
+                htmlBody = "<html><body>" + formulario + entradas_html \
+                    + "</body></html>"
             elif resource in self.short_url:
                 httpCode = "302 Redirect"
-                htmlBody = redirect_body.format("http://"+self.short_url[resource])
+                htmlBody = redirect_body.format("http://" \
+                    + self.short_url[resource])
             else:
                 httpCode = "404 Not Found"
                 htmlBody = "Not Found"
@@ -86,14 +91,16 @@ class contentApp (webapp.webApp):
 
             httpCode = "200 OK"
 
-            if not(resource_request.startswith('http://') or resource_request.startswith('https://')):
+            if not(resource_request.startswith('http://')
+            or resource_request.startswith('https://')):
                 requested_item = resource_request
                 resource_request = 'http://' + resource_request
             else:
-                requested_item = resource_request[resource_request.index('/') + 2:]
-            if not requested_item in self.long_url:
-                self.long_url[requested_item] = "/"+ str(len(self.short_url))
-                self.short_url["/"+ str(len(self.short_url))] = requested_item
+                requested_item =
+                resource_request[resource_request.index('/') + 2:]
+            if requested_item not in self.long_url:
+                self.long_url[requested_item] = "/" + str(len(self.short_url))
+                self.short_url["/" + str(len(self.short_url))] = requested_item
 
             htmlBody = url_pinchable.format(self.long_url[requested_item],
                                             self.long_url[requested_item],
